e4ec81812ca0df5905c3d9e6527e0588a49069c1
phiresky <phireskyde+git@gmail.com>
2025-03-05 18:29:58 +0100

    fix post install script taking 30s

--- bin/postinstall-script.cjs
+++ bin/postinstall-script.cjs
@@ -5,6 +5,9 @@ const zlib = require('node:zlib');
 const rimraf = require('rimraf');
 const tar = require('tar-fs');
 
+// skript takes 30s otherwise
+// https://github.com/nodejs/node/issues/47228 where the process also does not exit for 30 seconds.
+https.globalAgent.keepAlive = false;
 const destination = path.resolve(__dirname, '../src/pg-formatter');
 const secondaryDestination = path.resolve(__dirname, '../dist/pg-formatter');
 const libraryUrl = `https://github.com/darold/pgFormatter/archive/refs/tags/v5.5.tar.gz`;
